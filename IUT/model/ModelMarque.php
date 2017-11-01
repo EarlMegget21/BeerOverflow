5c141f1272cadab7a0d7ac643617aa656ee165ba
Unknown <papajr@outlook.fr>
2017-11-01 09:44:58 +0100

    légère modif de ModelMarque

--- IUT/model/ModelMarque.php
+++ IUT/model/ModelMarque.php
@@ -6,7 +6,7 @@ class ModelMarque extends Model
     private $id;
     private $nom;
     private $edition;
-    private $brasseur;
+    private $idBrasserie;
 	
 	public function __construct($id = NULL, $n = NULL, $ed = NULL, $br = NULL, $liste = NULL) {
         if (!is_null($id) && !is_null($n) && !is_null($ed) && !is_null($br) && !is_null($liste)) {
